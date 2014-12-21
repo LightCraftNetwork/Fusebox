dcf58c02ea3b40c67a6eb8c679f3d8def6632f2b
Ashy <ashle.thew@gmail.com>
2014-12-21 14:52:17 +1300

    Fixed core class

--- src/net/lightcraft/fusebox/Core.java
+++ src/net/lightcraft/fusebox/Core.java
@@ -1,13 +1,9 @@
 package net.lightcraft.fusebox;
 
-import java.util.ArrayList;
-
 import net.lightcraft.fusebox.util.effects.EffectManager;
 import net.lightcraft.fusebox.util.extra.ExtraManager;
 import net.lightcraft.fusebox.util.particle.ParticleManager;
 
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin{
@@ -17,6 +13,7 @@ public class Core extends JavaPlugin{
     @Override
     public void onEnable(){
     	main = this;
+    	init();
     }
     
     private void init(){
